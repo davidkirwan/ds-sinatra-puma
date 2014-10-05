ef24ac42cd96feec3a0b0ea25b4a99a7073d4f62
David Kirwan <dkirwan@tssg.org>
2014-10-05 01:47:54 +0100

    - Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+== Distributed Systems 2014 ==
+A Ruby application built in Sinatra/Puma for the Distributed Systems 2014 module from the
+MSc in Computing at the Waterford Insitute of Technology, Ireland
+
+=== Licence ===
+GNU 3.0
+
+=== Author ===
+David Kirwan
---
+++ app.rb
@@ -0,0 +1,46 @@
+$stdout.sync = true
+####################################################################################################
+# @author       David Kirwan https://github.com/davidkirwan/ds-sinatra-puma
+# @description  Ruby Sinatra/Puma app
+#
+# @date         2014-10-05
+####################################################################################################
+##### Require statements
+require 'sinatra/base'
+require 'puma'
+require 'logger'
+
+
+module Server
+class App < Sinatra::Base
+
+
+  ##### Variables
+  enable :static, :sessions, :logging
+  set :environment, :production
+  set :root, File.dirname(__FILE__)
+  set :public_folder, File.join(root, '/public')
+  set :views, File.join(root, '/views')
+  set :server, :puma
+
+  # Create the logger instance
+  set :log, Logger.new(STDOUT)
+  set :level, Logger::DEBUG
+  #set :level, Logger::INFO
+  #set :level, Logger::WARN
+
+  # Options hash
+  set :options, {:log => settings.log, :level => settings.level}
+
+  # 404 Controller
+  not_found do
+    [404, {"Content-Type" => "text/plain"},["404 Page Not Found"]]
+  end
+
+  get '/' do
+    erb :index
+  end
+
+
+end # End of the App class
+end # End of the Server module
